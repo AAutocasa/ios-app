2ae4b7c7cc0e3000ca980580d095640fa82a42bb
Enzo Maruffa <enzomm1999@gmail.com>
2021-05-23 14:57:34 -0300

    Initial commit

---
+++ Autocasa/AutocasaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutocasaApp.swift
+//  Autocasa
+//
+//  Created by Enzo Maruffa Moreira on 23/05/21.
+//
+
+import SwiftUI
+
+@main
+struct AutocasaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Autocasa/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Autocasa
+//
+//  Created by Enzo Maruffa Moreira on 23/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
